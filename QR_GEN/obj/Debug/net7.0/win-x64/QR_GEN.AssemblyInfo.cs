eba04d981bdd5a82b185228de95c20cb06dba3f2
Robin Proeckl <71661183+RoProe@users.noreply.github.com>
2025-02-01 16:15:41 +0100

    updated to net8.0

--- QR_GEN/obj/Debug/net7.0/win-x64/QR_GEN.AssemblyInfo.cs
+++ QR_GEN/obj/Debug/net7.0/win-x64/QR_GEN.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QR_GEN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf013f44f471c0b2c5546d89b6ac434ec80e04ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("QR_GEN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QR_GEN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
